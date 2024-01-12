70e3d1635dae11e7955993b0e84e36c5b03c597d
jesulayomy <jesulayomi081@gmail.com>
2024-01-12 18:30:22 +0100

    [App]: Created counter app

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  return <MainApp />;
+}
+
+function MainApp() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, World</Text>
+      <Text style={styles.number}>{count}</Text>
+      <CounterButton incrementCount={incrementCount} />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+function CounterButton({ incrementCount }) {
+  return (
+    <TouchableOpacity onPress={incrementCount} style={styles.button}>
+      <Text style={styles.buttonText}>Increment</Text>
+    </TouchableOpacity>
   );
 }
 
@@ -16,5 +37,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  number: {
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
