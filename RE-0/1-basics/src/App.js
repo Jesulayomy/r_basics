a44512fecf8735cd12ce6c9bfe79aad0d29dbc6c
Jesulayomy <jesulayomi081@gmail.com>
2024-08-11 20:17:30 +0100

    [Restart]: Zero Time

---
+++ RE-0/1-basics/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        App
+      </header>
+      <p>Hello App</p>
+    </div>
+  );
+}
+
+export default App;
